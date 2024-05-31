df5a69b25bb1100d3b72a75f2be5d1176356dbee
g.rithvik <g.rithvik909@gmail.com>
2024-05-31 14:45:37 +0530

    4th updated

--- assignment 2 (30-05-2024).sql
+++ assignment 2 (30-05-2024).sql
@@ -53,12 +53,12 @@ select * from company_bonus where (service_type=1 and
 
 ------ 4th question assignment2---------
 WITH cte AS (
-    SELECT id,service_type, age, concat(datediff(year,date_of_join,getdate()),' years') as 'service_status',
+    SELECT id,name,service_type, age, dbo.age(date_of_join,GETDATE()) as 'service_status',
         row_number() OVER (partition by service_type ORDER BY age) rnk_min,
         row_number() OVER (partition by service_type ORDER BY age DESC) rnk_max
     FROM company_bonus
 )
-SELECT id,service_type, age,service_status
+SELECT id,name,service_type, age,service_status
 FROM cte
 WHERE rnk_min = 1 OR rnk_max = 1
 ORDER BY service_type,age;
